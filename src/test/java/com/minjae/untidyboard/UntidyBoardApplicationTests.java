9941df279e8c5cade092662c8fee8ccc15c52b9d
Min-92 <0201.mj.lee@gmail.com>
2020-03-09 17:44:27 +0900

    feat : project init
    - gradle, spring boot 프로젝트 생성
    - close #1

---
+++ src/test/java/com/minjae/untidyboard/UntidyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjae.untidyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntidyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
