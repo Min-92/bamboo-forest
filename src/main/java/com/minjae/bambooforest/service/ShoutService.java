1ca366e4d3ee23c6e8df123ab94de2ba648a3b43
Min-92 <0201.mj.lee@gmail.com>
2020-03-15 18:46:37 +0900

    feat : ShoutService 작성
    - close #5
    - save, load 메소드 작성
    - save() : requestDto를 entity로 변경 후 db에 저장, 해당 entity 리턴
    - load() : 저장소 내부 모든 entity 불러옴, responseDto로 변환 후 리턴

--- src/main/java/com/minjae/bambooforest/service/ShoutService.java
+++ src/main/java/com/minjae/bambooforest/service/ShoutService.java
@@ -1,18 +1,38 @@
 package com.minjae.bambooforest.service;
 
 import com.minjae.bambooforest.domain.Shout;
+import com.minjae.bambooforest.domain.repository.ShoutRepository;
 import com.minjae.bambooforest.dto.ShoutRequestDto;
 import com.minjae.bambooforest.dto.ShoutResponseDto;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ShoutService {
-	public ShoutResponseDto save(ShoutRequestDto dto){
+
+	private ShoutRepository shoutRepository;
+
+	public ShoutService(ShoutRepository shoutRepository) {
+		this.shoutRepository = shoutRepository;
+	}
+
+	@Transactional
+	public Shout save(ShoutRequestDto dto) {
 		Shout shout = dto.toEntity();
 		shout.generate();
+		return shoutRepository.save(shout);
+	}
 
-		// todo : db에 데이터 save, responseDto return;
+	@Transactional(readOnly = true)
+	public List<ShoutResponseDto> Load() {
+		List<ShoutResponseDto> result = new ArrayList();
 
-		return new ShoutResponseDto();
+		shoutRepository.findAll().forEach(shout -> {
+			result.add(new ShoutResponseDto(shout));
+		});
+
+		return result;
 	}
 }
