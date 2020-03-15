67b917adfc3ad3bcd16feca03283f978ad2e3cd0
Min-92 <0201.mj.lee@gmail.com>
2020-03-15 18:56:15 +0900

    feat : ShoutResponseDto 생성자 추가
    - Shout 객체를 매개변수로 받는 생성자 추가

--- src/main/java/com/minjae/bambooforest/dto/ShoutResponseDto.java
+++ src/main/java/com/minjae/bambooforest/dto/ShoutResponseDto.java
@@ -1,8 +1,15 @@
 package com.minjae.bambooforest.dto;
 
+import com.minjae.bambooforest.domain.Shout;
+
 public class ShoutResponseDto {
+
 	private String content;
+
 	//todo : 좌표, 시간 등 필요한 정보 추가
+	public ShoutResponseDto(Shout shout) {
+		this.content = shout.getContent();
+	}
 
 	public String getContent() {
 		return content;
